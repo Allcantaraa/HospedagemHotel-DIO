3e706178a90c53df5401187f982fb0062fe099d1
Allcantaraa <kauayt120@gmail.com>
2024-09-21 19:29:09 -0300

    Feito

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using DesafioProjetoHospedagem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p3 = new Pessoa(nome: "Kaua Alcantara");
+Pessoa p4 = new Pessoa(nome: "Arroznaldo");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+hospedes.Add(p3);
+hospedes.Add(p4);
+
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 4, valorDiaria: 180);
+
+Reserva reserva = new Reserva(diasReservados: 55);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
